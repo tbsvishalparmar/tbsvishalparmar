bae4bb9fcdc4e189be102349865ebdd4fdec2993
nazifha <tbs.nazifha@gmail.com>
2022-01-06 16:02:18 +0530

    Day 3
    Signed-off-by: nazifha <tbs.nazifha@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_tutorial/pages/home_page.dart';
+import 'package:flutter_tutorial/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,13 +12,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepOrange),
+      routes: {
+        "/": (contaxt) => LoginPage(),
+        "/login": (context) => LoginPage(),
+        "/home": (context) => HomePage()
+      }, 
     );
   }
-
-void bringVegitables(){
-  
-}
-
-
 }
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Center(
+        child: Container(
+          child: Text("Login Page..."),
+        ),
+      ),
+    );
+  }
+}
